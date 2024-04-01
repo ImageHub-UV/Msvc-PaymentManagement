e4ac65ac066f6a8114d35c4d989d9775bc649d05
Seb0927 <sebastianidrobo@hotmail.com>
2024-04-01 18:00:10 -0500

    Generation of project

---
+++ src/main/java/com/imagehub/payment_management/PaymentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.imagehub.payment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagehub/payment_management/PaymentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagehub.payment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
